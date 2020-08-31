a6ac2463873f0cfb2f6aa75c63bdfbbaa30c2579
= <garvitgalgat@gmail.com>
2020-08-31 16:26:42 +0530

    button press fades all items

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'dart:async';
+
 double width, height;
 String userSelection;
+//visibility of gifs
+bool rock = true, paper = true, scissors = true;
 
 void main() {
   runApp(MaterialApp(
     home: Splash(),
   ));
 }
-
 
 class Splash extends StatefulWidget {
   @override
@@ -18,14 +20,13 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   @override
-  void initState(){
-    Timer(Duration(seconds: 2),
-            ()=>Navigator.pushReplacement(
-            context,MaterialPageRoute(
-            builder:(context) => Home()
-        )
-        ));
+  void initState() {
+    Timer(
+        Duration(milliseconds: 1200),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Home())));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,49 +39,37 @@ class _SplashState extends State<Splash> {
   }
 }
 
-void calc(String userSelection){
-  int bot = Random().nextInt(3), userItem;;
+void calc(String userSelection) {
+  int bot = Random().nextInt(3), userItem;
+
   String botSelection;
 
-  if(bot == 0)
-    botSelection = 'rock';
-  if(bot == 1)
-    botSelection = 'paper';
-  if(bot == 2)
-    botSelection = 'scissors';
+  if (bot == 0) botSelection = 'rock';
+  if (bot == 1) botSelection = 'paper';
+  if (bot == 2) botSelection = 'scissors';
 
   print("Bot selected $botSelection");
   //0 = rock, 1 = paper, 2 = scissors
-  if(userSelection == 'rock')
-    userItem = 0;
-  if(userSelection == 'paper')
-    userItem = 1;
-  if(userSelection == 'scissors')
-    userItem = 2;
+  if (userSelection == 'rock') userItem = 0;
+  if (userSelection == 'paper') userItem = 1;
+  if (userSelection == 'scissors') userItem = 2;
 
-  if(bot == userItem)
+  if (bot == userItem)
     print("Draw");
-
-  else{
-    if(userItem == 0){
-      if(bot == 1)
-        print("You lose!! $botSelection covers $userSelection");
-      if(bot == 2)
-        print("You win!! $userSelection smashes $botSelection");
+  else {
+    if (userItem == 0) {
+      if (bot == 1) print("You lose!! $botSelection covers $userSelection");
+      if (bot == 2) print("You win!! $userSelection smashes $botSelection");
     }
 
-    if(userItem == 1){
-      if(bot == 0)
-        print("You win!! $userSelection covers $botSelection");
-      if(bot == 2)
-        print("You lose!! $botSelection cut $userSelection");
+    if (userItem == 1) {
+      if (bot == 0) print("You win!! $userSelection covers $botSelection");
+      if (bot == 2) print("You lose!! $botSelection cut $userSelection");
     }
 
-    if(userItem == 2){
-      if(bot == 0)
-        print("You lose!! $botSelection smashes $userSelection");
-      if(bot == 1)
-        print("You win!! $userSelection cut $botSelection");
+    if (userItem == 2) {
+      if (bot == 0) print("You lose!! $botSelection smashes $userSelection");
+      if (bot == 1) print("You win!! $userSelection cut $botSelection");
     }
   }
 }
@@ -90,27 +79,20 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class CustomCard extends StatelessWidget {
-  String item;
-  CustomCard({this.item});
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        print("Statement");
-        userSelection = item;
-        calc(item);
-      },
-      child: Card(
-        elevation: 0,
-        child: Image.asset('assets/$item.gif',height: height*0.30, width: width/4),
-      ),
-    );
-  }
-}
-
 class _HomeState extends State<Home> {
+  bool firstLayerVisible = true;
+  bool secondLayerVisible = false;
+  bool userItem = false;
+  bool botItem = false;
 
+//  AnimationController _animationController;
+//
+//  @override
+//  void initState() {
+//    _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
+//    _animationController.forward();
+//    super.initState();
+//  }
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
@@ -119,18 +101,85 @@ class _HomeState extends State<Home> {
       backgroundColor: Color.fromRGBO(255, 255, 255, 1),
       body: Column(
         children: [
-          SizedBox(height: height*0.05,),
-          Text('RockPaperScissors', style: TextStyle(fontFamily: 'KaushanScript', fontSize: 38),),
-          SizedBox(height: height*0.2,),
-          Text('SELECT', style: TextStyle(fontFamily: 'KaushanScript', fontSize: 28),),
-          SizedBox(height: height*0.05,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomCard(item: 'rock'),
-              CustomCard(item: 'paper'),
-              CustomCard(item: 'scissors'),
-            ],
+          SizedBox(
+            height: height * 0.05,
+          ),
+          Text(
+            'RockPaperScissors',
+            style: TextStyle(fontFamily: 'KaushanScript', fontSize: 38),
+          ),
+          SizedBox(
+            height: height * 0.2,
+          ),
+          Text(
+            'SELECT',
+            style: TextStyle(fontFamily: 'KaushanScript', fontSize: 28),
+          ),
+          SizedBox(
+            height: height * 0.05,
+          ),
+          AnimatedOpacity(
+            opacity: firstLayerVisible ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 500),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      firstLayerVisible = false;
+                      Timer(Duration(milliseconds: 600), () {
+                        print("inside");
+                        setState(() {
+                          secondLayerVisible = true;
+                        });
+                      });
+                    });
+                  },
+                  child: Card(
+                    elevation: 0,
+                    child: Image.asset('assets/rock.gif',
+                        height: height * 0.30, width: width / 4),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      firstLayerVisible = false;
+                      Timer(Duration(milliseconds: 600), () {
+                        print("inside");
+                        setState(() {
+                          secondLayerVisible = true;
+                        });
+                      });
+                    });
+                  },
+                  child: Card(
+                    elevation: 0,
+                    child: Image.asset('assets/paper.gif',
+                        height: height * 0.30, width: width / 4),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      firstLayerVisible = false;
+                      Timer(Duration(milliseconds: 600), () {
+                        print("inside");
+                        setState(() {
+                          secondLayerVisible = true;
+                        });
+                      });
+                    });
+                  },
+                  child: Card(
+                    elevation: 0,
+                    child: Image.asset('assets/scissors.gif',
+                        height: height * 0.30, width: width / 4),
+                  ),
+                ),
+              ],
+            ),
           ),
 
         ],
@@ -138,3 +187,17 @@ class _HomeState extends State<Home> {
     );
   }
 }
+//InkWell(
+//onTap: () {
+//print("Statement");
+//userSelection = item;
+//calc(item);
+//
+//
+//
+//},
+//child: Card(
+//elevation: 0,
+//child: Image.asset('assets/$item.gif',
+//height: height * 0.30, width: width / 4),
+//)
